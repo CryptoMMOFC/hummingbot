e2885c4e9f3b0fbe65c37abb8ec376b0c55780cf
Terence Schofield <tschof@gmail.com>
2021-10-24 19:10:35 +0000

    copied pure_market_maker and prefixed with cts

---
+++ hummingbot/strategy/cts_pure_market_making/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from .cts_pure_market_making import CtsPureMarketMakingStrategy
+from .inventory_cost_price_delegate import InventoryCostPriceDelegate
+__all__ = [
+    CtsPureMarketMakingStrategy,
+    InventoryCostPriceDelegate,
+]
